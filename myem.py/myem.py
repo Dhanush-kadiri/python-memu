3c4ca5ef1a608b6149499be899942052fbcbe47f
Dhanush-kadiri <150281871+Dhanush-kadiri@users.noreply.github.com>
2024-09-17 17:29:35 +0530

    Update myem.py

--- myem.py/myem.py
+++ myem.py/myem.py
@@ -7,7 +7,8 @@ from difflib import SequenceMatcher
 from io import BytesIO
 
 app = Flask(__name__)
-CORS(app, origins=['http://localhost:3000', 'http://192.168.0.101',"https://test1-inky-three.vercel.app"])
+CORS(app, origins=['http://localhost:3000', 'http://192.168.0.101'])
+CORS(app, resources={r"/api/*": {"origins": "https://test1-inky-three.vercel.app"}})
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/myem'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
